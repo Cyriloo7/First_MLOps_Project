6b50a2ab46103412997ac68bc3fe36eca773aac1
cyril jose <cyril.jose2020@vitstudent.ac.in>
2024-07-06 19:54:53 +0530

    updated

--- template.py
+++ template.py
@@ -5,7 +5,7 @@ from pathlib import Path
 list_of_files = [
     ".github/workflows/.gitkeep",
     "src/__init__.py",
-    "sec/components/__init__.py",
+    "src/components/__init__.py",
     "src/components/data_ingestion.py",
     "src/components/data_transformation.py",
     "src/components/model_trainer.py",
