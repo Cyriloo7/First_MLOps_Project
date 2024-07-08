056d8f99aefb178c708d6c7d7d4b5e96b7865239
cyril jose <cyril.jose2020@vitstudent.ac.in>
2024-07-08 16:19:34 +0530

    updated

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('form .html')
+    return render_template('index .html')
 
 @app.route('/predict', methods=['POST','GET'])
 def predict():
@@ -30,4 +30,4 @@ def predict():
         return render_template("result.html", final_result = final_result)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
