6646618cd6fd6dbc3967a416b7eceb69c6aed6f4
cyril jose <cyril.jose2020@vitstudent.ac.in>
2024-07-08 16:10:06 +0530

    updated

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, jsonify, request
+from src.pipeline.prediction_pipeline import PredictPipeline, CustomData
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('form .html')
+
+@app.route('/predict', methods=['POST','GET'])
+def predict():
+    if request.method == 'GET':
+        return render_template('form.html')
+    else:
+        data = CustomData(
+            carat=float(request.form.get("carat")),
+            depth=float(request.form.get("depth")),
+            table=float(request.form.get("table")),
+            x=float(request.form.get("x")),
+            y=float(request.form.get("y")),
+            z=float(request.form.get("z")),
+            cut=request.form.get("cut"),
+            color=request.form.get("color"),
+            clarity=request.form.get("clarity")
+        )
+        final_data = data.get_data_as_dataframe()
+        pipeline = PredictPipeline()
+        prediction = pipeline.predict(final_data)
+        final_result = round(prediction[0], 2)
+        return render_template("result.html", final_result = final_result)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
